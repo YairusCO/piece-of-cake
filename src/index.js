6bca2f1cdfd163bca6cbdde97bc50d215a49e966
ArseniyX <jkl@usa.com>
2022-09-11 10:26:34 +0300

    add redux and prettier

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./components/App";
-import i18n from "./i18n";
-import { I18nextProvider } from "react-i18next";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './components/App'
+import i18n from './i18n'
+import { I18nextProvider } from 'react-i18next'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<Provider store={store}>
+			<I18nextProvider i18n={i18n}>
+				<App />
+			</I18nextProvider>
+		</Provider>
+	</React.StrictMode>
+)
