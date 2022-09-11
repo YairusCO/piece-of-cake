457b189b463cd67ee232f9951b7a766c411a6d6f
yairus <yair@pwm.co.il>
2022-09-11 11:03:05 +0300

    remove import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,31 @@
 import React, { useEffect } from 'react'
-import { BrowserRouter  , Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../Home'
 import Header from '../Header'
-import Intro from '../Intro'
 import Layout from '../Layout'
 import SelectIngredient from '../NewBake/SelectIngredient'
 import history from '../../services/history'
-import {ADD, HOME, SELECT_INGREDIENT} from '../../util/routes'
+import { HOME, SELECT_INGREDIENT } from '../../util/routes'
 function App() {
-  return (
-    <div className="app"> 
-      <Header/>
-      <div className="center">
-      <Layout>
-      <BrowserRouter history={history}>
-          <Routes>
-        <Route exact path={HOME} element={<Home/>} />
-        <Route exact path={ADD} element={<Intro/> } />
-        <Route exact path={SELECT_INGREDIENT} element={<SelectIngredient/> } />
-          </Routes>
-        </BrowserRouter>
-        </Layout>
-    </div>
-    </div>
-  )
+	return (
+		<div className="app">
+			<Header />
+			<div className="center">
+				<Layout>
+					<BrowserRouter history={history}>
+						<Routes>
+							<Route exact path={HOME} element={<Home />} />
+							<Route
+								exact
+								path={SELECT_INGREDIENT}
+								element={<SelectIngredient />}
+							/>
+						</Routes>
+					</BrowserRouter>
+				</Layout>
+			</div>
+		</div>
+	)
 }
 
 export default App
--- src/util/routes.js
+++ src/util/routes.js
@@ -1,9 +1,6 @@
-
 export const DEFAULT = '*'
 export const HOME = '/'
-export const ADD = '/add'
 export const SELECTED = '/selected'
 export const SELECT_INGREDIENT = '/select-ingredient'
 
 const DEFAULT_ROUTES = ['']
-
